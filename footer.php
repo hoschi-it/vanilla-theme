4e1e76987b497b9f65ce4d9349b30bc33ed27714
hoschi-it <hoschi-it@web.de>
2019-05-12 16:44:01 +0200

    customize menu for header and footer

--- footer.php
+++ footer.php
@@ -1,13 +1,12 @@
     <footer>
-        <nav>
-            <ul>
-                <li>Impressum</li>
-                <li>Datenschutz</li>
-                <li>Einloggen</li>
-            </ul>
-        </nav>
+        <?php wp_nav_menu(
+            array(
+                'theme_location' => 'footer-menu',
+                'container' => 'nav'
+            )            
+        ); ?>
         <p>Copyright &copy;2019 FeG Bad Vilbel. Alle Rechte vorbehalten.</p>
     </footer>
-
+    <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -2,21 +2,24 @@
 <html lang="<?php bloginfo('language'); ?>">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php bloginfo('description'); ?></title>
+    <title><?php bloginfo('name'); ?></title>
     <link
         rel="stylesheet" 
         type="text/css" 
         href="<?php bloginfo('stylesheet_url'); ?>"
         >
+    <?php wp_head(); ?>
 </head>
 <body>
     <header>
         <h1><?php bloginfo('name');?></h1>
         <p><?php bloginfo('description');?></p>
     </header>
-<?php wp_nav_menu( array(
-    'theme_location' => 'header_menu', 
-    'container'=>'nav', 
-    'container_class'=>'mainnav'
-)); 
+<?php wp_nav_menu( 
+    array(
+        'theme_location' => 'header-menu', 
+        'container'=>'nav', 
+        'container_class'=>'mainnav'
+    )
+); 
 ?>
