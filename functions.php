c006c9638fa82adbf1fcc34c1679a51073eda79b
hoschi-it <hoschi-it@web.de>
2019-05-18 14:47:24 +0200

    customizable sidebar

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-class VanTheme {
+class VanillaTheme {
 
     public static function register_menus(){
         register_nav_menus(
@@ -11,6 +11,20 @@ class VanTheme {
         );
     }
 
+    public static function widgets_init() {
+        register_sidebar(
+            array(
+                'name'          => __('Primary Sidebar'),
+                'id'            => 'sidebar-0',
+                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+                'after_widget'  => '</aside>',
+                'before_title'  => '<h3 class="widget-title">',
+                'after_title'   => '</h3>',
+            )
+        );
+    }
+
 }
 
-add_action('init', array('VanTheme', 'register_menus'));
+add_action('init', array('VanillaTheme', 'register_menus'));
+add_action('widgets_init', array('VanillaTheme', 'widgets_init'));
--- sidebar.php
+++ sidebar.php
@@ -1,18 +1,8 @@
-    <aside>
-        <section>
-            <h4>Gottesdienste</h4>
-            <p>Sonntags, 11:00 - 12:30 Uhr, 
-                Johann-Strauß-Straße 1, Dortelweil
-            </p>
-        </section>
+<?php 
+$sidebar = 'sidebar-0';
 
-        <section>
-            <h4>Gemeinde-Kalender</h4>
-            <!--iframe
-                src="https://fegbv.church.tools/?q=churchcal&embedded=true&viewname=eventView&entries=4&hideFooter=true&minical=true"
-                height="500px"
-                width="100%"
-            >
-            </iframe-->
-        </section>
-    </aside>
+if( is_active_sidebar($sidebar) ):
+    dynamic_sidebar('sidebar-0'); 
+endif;
+
+?>    
